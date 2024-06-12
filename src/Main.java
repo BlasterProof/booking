2968af5bad238d34d96abe3bb251e2d189ce3420
Sheila Santoyo <gonvisor@live.com>
2024-06-11 20:15:14 -0600

    funcion suma agregada

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import math.Calculator;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
+
+
+        Calculator c = new Calculator();
+
+        c.sum(12,12);
     }
 }
